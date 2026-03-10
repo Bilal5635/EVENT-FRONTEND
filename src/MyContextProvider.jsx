1d147bd846c2d9fecf382e9b85c47f48bd023aea
Bilal5635 <mhdbilal1166@gmail.com>
2026-03-10 10:19:24 +0530

    2nd commit

--- src/MyContextProvider.jsx
+++ src/MyContextProvider.jsx
@@ -5,7 +5,8 @@ export const UserContext=createContext();
 
 export default function MyContextProvider({children}) {
 
-    const host= "http://localhost:8100" // port number as per backend
+    // const host= "http://localhost:8100" // port number as per backend
+    const host = "https://eventserver-2ubi.onrender.com"
 
   return (
     <div>
